fad70bae175ac9d907d678507a06ae273d6b5902
kivindu-lakshan <kivindulakshan@gmail.com>
2026-03-25 20:01:53 +0530

    feat : fixed renderring issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,14 +9,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/" element={<HomePage />} />
 
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/verify-email/:token" element={<VerifyEmailPage />} />
         <Route path="/home" element={<HomePage />} />
 
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
